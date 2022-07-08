f7d4e6451bdb4c4ada48ef6f2719bbfb6df32047
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-08 21:05:40 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let str = '+5-2+4-m/n*2:4';
+    let arr = ['+','-','*'];
+    let count = 0;
+    for(let i = 0;i<str.length;i++){
+       if(str.includes(arr[i])){
+        count++
+       }
+    }
+    console.log(count);
+    console.log(str);
