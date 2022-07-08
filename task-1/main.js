98bb350253799b0a4c00a4f74a9649ae59af8aac
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-08 19:42:12 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt');
-prompt.start();
+let str = '31,15,40';
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let strarr = str.split(',');
+let min = strarr[0];
+for(let i = 0;i<strarr.length;i++){
+    if(strarr[i]<min){
+        min = strarr[i];
+    }
+}
+console.log(min);
