f641ccd71b7fcfcd2de57984671e0d0ece0b2deb
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-08 21:30:49 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt');
-prompt.start();
+let  str = 'Hello world! Hello,    country!';
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let newarr = ['','!',',',', '];
+let count = 0;
+for(let i = 0;i<str.length;i++){
+    if(str.includes(newarr[i])){
+        count++
+    }
+}
+console.log(count);
