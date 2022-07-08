e7546fb169846a49af4a58258c5a09c23b53d4d1
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-08 20:03:42 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+       let num = '328';
+    let arr = num.split('');
+    // console.log(arr);
+
+    let max =arr[0];
+    for(let i = 0;i<arr.length;i++){
+        // console.log(arr[i]);
+        if(arr[i]>max){
+            max = arr[i];
+        }
+
+    }
+    console.log(max);
   
 });
