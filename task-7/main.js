5325f9f3852fc16d0a5b62553e6c819355197d98
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-10 01:25:09 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -6,4 +6,17 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+      let cars = 'kpkkkpzkk';
+    let count = 0;
+    let arr = cars.split('')
+    console.log(arr);
+    for(let i = 0;i<arr.length;i++){
+        // console.log(arr[i]); 
+        // debugger
+        if(cars[i] == ['k'] && cars[i+1] == ['k']){
+            count++;
+        }
+    }
+    console.log(count);
+  
 });
