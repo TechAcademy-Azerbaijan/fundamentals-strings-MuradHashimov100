7d349a11fbfe37015d941bcc503c145c0a257d3b
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-10 00:26:36 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,26 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+      let str = '255222'; 
+    let arr = str.split('');
+    console.log(arr);
+    let count1=0;
+    let count2=0;
+    for(let i = 0;i<str.length;i++){
+        // console.log(arr[i]);
+        if(str[i] == '5'){
+            count1++;
+        }
+        else{
+            count2++;
+        }
+    }
+    console.log(count1,count2);
+    if(count1>count2){
+        console.log('5')
+    }
+    else{
+        console.log('2')
+    }
   
 });
