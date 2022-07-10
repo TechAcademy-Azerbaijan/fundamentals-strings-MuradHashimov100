5c28537226bee8818c4ce8a5e6a484df4b64426f
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-10 23:08:40 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -4,3 +4,11 @@ let s = "This is a cat sitting on a table"
 let c = "t"
 
 // Write code here. 
+let str = "This is a cat sitting on a table";
+    let count = 0;
+    for(let i = 0;i<str.length;i++){
+        if(str[i] =='T' || str[i] == 't'){
+            count++;
+        }
+    }
+    console.log(count);
