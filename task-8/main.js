cdfa09c7f4a7780efd71f4e3ff0b3f07f3266977
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-10 13:13:00 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let str = 'introduction to algorithms';
+    let arr = str.split(' ')
+     console.log(arr);
+    let newArr = [];
+    for(let i = 0;i<arr.length;i++){
+       console.log(arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1))
+    }
+   console.log(arr);
+   const str2 = arr.join(" ");
+   console.log(str2);
   
 });
