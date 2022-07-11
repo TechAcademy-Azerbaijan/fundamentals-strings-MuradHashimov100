fc4b7ad58ee958ff20c2321e83ba617221c2fd52
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-11 11:30:36 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -5,5 +5,24 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let str = 'I am programming on Javascript.';
+ let newArr = [];
+let firstIndex,lastIndex;
+ for(let i = 0;i<str.length;i++){
+    if(str[i] == ' '){
+        newArr.push(i);
+    }
+ }
+ lastIndex = newArr[newArr.length-1];
+
+
+ console.log(newArr,lastIndex);
+ if(newArr.length>0){
+    console.log(newArr[0]);
+    console.log(lastIndex);
+ }
+ else{
+    console.log('-1');
+ }
   
 });
